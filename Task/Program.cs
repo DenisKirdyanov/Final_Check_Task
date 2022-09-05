757b8fda1ec4c5b4b2b00b3b93a982245318af22
Denis Kirdyanov <pilsdenis@yandex.ru>
2022-09-05 18:39:48 +0300

    Процесс решения

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,5 +4,40 @@
 // лучше обойтись исключительно массивами.
 
 
-Console.WriteLine("Hello, World!");
-Final_checking_work
+Console.WriteLine("Введите количество элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+
+string[] GetArray(int n)
+{
+    string[] result = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива:");
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write($" {inArray[i]} ");
+    }
+}
+
+string[] array = GetArray(number);
+
+Console.WriteLine("Ваш массив : ");
+PrintArray(array);
+
+Console.WriteLine();
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   if (array[i].Length == 3) j = j + 1;
+}
+
+Console.WriteLine(j);
